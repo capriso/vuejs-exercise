01ee09ddf26910572fc952a053db2a0398cdf053
Teddy <capriso@gmail.com>
2018-04-03 22:39:57 +0900

    implement TodoInput component

--- practice/vue-todo/src/components/TodoInput.vue
+++ practice/vue-todo/src/components/TodoInput.vue
@@ -1,12 +1,61 @@
 <template>
-  <div>input</div>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" placeholder="Type what you have to do">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fa fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      newTodoItem: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem !== "") {
+        var value = this.newTodoItem && this.newTodoItem.trim();
 
+        // 로컬 스토리지에 데이터를 추가
+        // (key, value) pair로 저장하는데,
+        // 이 예제에서는 최대한 단순화하기 위해 key와 value를 동일하게 구성
+        localStorage.setItem(value, value);
+        this.clearInput();
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+  input:focus {
+    outline: none
+  }
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: inline-block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
 </style>
